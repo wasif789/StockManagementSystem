c76d2ece785b9f19a96b4c2dbc4115d6ce854a68
wasif farid <wsffarid1@gmail.com>
2021-09-22 18:31:21 +0530

    stock management system

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StockManagementSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
